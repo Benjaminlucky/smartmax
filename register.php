cdc4679e93e892b9bd5357916a02224702c7df72
Benjaminlucky <42000766+Benjaminlucky@users.noreply.github.com>
2021-08-01 13:28:38 +0100

    first commit

---
+++ register.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register - Smartmax</title>
+</head>
+<body>
+    
+
+    <div class="container"> <!-- Overall Container-->
+
+        <div class="wrapper"> <!-- Overall Wrapper--->
+
+
+            <h1>Register as a Smartmax Realtor</h1>
+            <p>FILL ALL INFORMATIONS CAREFULLY</p>
+
+            <form action="">
+
+                <input type="text" name="first_name" placeholder="Lucky" REQUIRED>
+
+                <input type="text" name="last_name" placeholder="Benjamin" REQUIRED>
+
+                <input type="text" name="phone_number" placeholder="09021359415" REQUIRED>
+
+                <input type="email" name="email" placeholder="smartmax@gmail.com" REQUIRED>
+
+                <input type="text" name="bank_name" placeholder="Bank Name" REQUIRED>
+
+                <input type="text" name="account_number" placeholder="Account Number" REQUIRED>
+
+                <input type="submit" value="register">
+
+
+            </form>
+
+        </div>
+
+
+    </div>
+
+
+
+
+
+</body>
+</html>
